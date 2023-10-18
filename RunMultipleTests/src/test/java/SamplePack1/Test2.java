3d2aacdad32dafdb4b084f5518e3194884857443
ShielaMarie1 <147574366+ShielaMarie1@users.noreply.github.com>
2023-10-18 10:54:57 +0800

    First commit.

---
+++ RunMultipleTests/src/test/java/SamplePack1/Test2.java
@@ -0,0 +1,17 @@
+package SamplePack1;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+	
+	@Test
+		public void m1Test() {
+			System.out.println("Package1 Regression test 1");
+		}
+		
+	@Test
+		public void m2Test() {
+			System.out.println("Package1 Regression test 2");  
+		}
+  
+}
---
+++ RunMultipleTests/src/test/java/SamplePack2/Test1.java
@@ -0,0 +1,17 @@
+package SamplePack2;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+		public void m1Test() {
+			System.out.println("Package2 Other tests 1");
+		}
+		
+	@Test
+		public void m2Test() {
+			System.out.println("Package2 Other tests 2");  
+		}
+  
+}
